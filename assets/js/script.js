c489c61977375a932eff24edc0dd36a00b108203
Claudia <claudia.araya.ro@gmail.com>
2022-12-05 14:46:46 -0300

    mejorando funcionalidad de archivo js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,30 @@
 //  smooth scroll
-$(document).ready(function () {
-    $("a").click(function () {
-        var cat = this.hash
-        $("html, body").animate({
-                scrollTop: $(cat).offset().top
-            },
-            800
-        )
-    })
-})
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+            window.location.hash = hash;
+            });
+        }
+    });
+});
+    
+// $(document).ready(function () {
+//     $("a").click(function () {
+//         var cat = this.hash
+//         $("html, body").animate({
+//                 scrollTop: $(cat).offset().top
+//             },
+//             800
+//         )
+//     })
+// })
 
 // tooltips
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
